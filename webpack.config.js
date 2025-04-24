3ecf97beada8fb0a7fc308a693e9ee7710211ca1
Jerry Bruwes <jbruwes@icloud.com>
2025-04-24 13:12:31 +0200

    modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 module.exports = {
 		mode: 'production',
 		experiments: { outputModule: true },
-		externals: ['@vue/compiler-sfc', '@vue/babel-plugin-jsx'],
+		externals: ['@vue/compiler-sfc'],
 		entry: [ path.resolve(__dirname, 'src/index.ts') ],
 		output: {
 			path: path.resolve(__dirname, 'dist'),
