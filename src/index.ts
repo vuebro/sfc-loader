93d1dcf1e95ebb597a04b35e3e0d9d5a234627b1
Jerry Bruwes <jbruwes@icloud.com>
2025-05-03 17:41:52 +0200

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import type {
+  CompilerError,
+  CompilerOptions,
   SFCDescriptor,
+  SFCScriptBlock,
   SFCStyleBlock,
-  SFCTemplateCompileOptions,
 } from "@vue/compiler-sfc";
+import type { Transform } from "sucrase";
 
 import {
   compileScript,
@@ -12,99 +15,111 @@ import {
 } from "@vue/compiler-sfc";
 import { useStyleTag } from "@vueuse/core";
 import hash from "hash-sum";
+import { transform } from "sucrase";
 
+const log = (msgs: (CompilerError | Error | string)[]) => {
+  msgs.forEach((msg) => {
+    console.log(msg);
+  });
+};
 const addStyle = async (
-  id: string,
-  { filename }: SFCDescriptor,
-  { content, scoped = false, src }: SFCStyleBlock,
-) => {
-  const { code, errors } = await compileStyleAsync({
-    filename,
-    id,
-    isProd: true,
-    scoped,
-    source: src ? await (await fetch(src)).text() : content,
-  });
-  errors.forEach((error) => {
-    console.error(error);
-  });
-  useStyleTag(code, { ...(id && { id }) });
-};
-const loadModule = async (filename: string) => {
-  const module: Record<string, object | string> = {};
-  const { descriptor } = parse(await (await fetch(filename)).text(), {
-    filename,
-    sourceMap: false,
-  });
-  const id = `data-v-${hash(filename)}`;
-  const scoped = descriptor.styles.some(({ scoped }) => scoped);
-  if (scoped) module.__scopeId = id;
-  const templateOptions: SFCTemplateCompileOptions | undefined =
-    descriptor.template
-      ? {
-          compilerOptions: {
-            mode: "module",
-            scopeId: scoped ? id : null,
-          },
-          filename: descriptor.filename,
-          id,
-          isProd: true,
-          scoped,
-          slotted: descriptor.slotted,
-          source: descriptor.template.src
-            ? await (await fetch(descriptor.template.src)).text()
-            : descriptor.template.content,
-        }
-      : undefined;
-  if (descriptor.script || descriptor.scriptSetup) {
-    if (descriptor.script?.src)
-      descriptor.script.content = await (
-        await fetch(descriptor.script.src)
-      ).text();
-    const { bindings, content } = compileScript(descriptor, {
-      // babelParserPlugins: ["jsx", "typescript"],
+    id: string,
+    { filename }: SFCDescriptor,
+    { content, module, scoped = false, src }: SFCStyleBlock,
+  ) => {
+    const { code, errors } = await compileStyleAsync({
+      filename,
       id,
-      inlineTemplate: false,
-      isProd: true,
-      ...(templateOptions && { templateOptions }),
+      modules: !!module,
+      scoped,
+      source: src ? await (await fetch(src)).text() : content,
     });
-    if (templateOptions?.compilerOptions && bindings)
-      templateOptions.compilerOptions.bindingMetadata = bindings;
-    Object.assign(
-      module,
-      (
-        (await import(
-          `data:text/javascript;base64,${btoa(
-            Array.from(new TextEncoder().encode(content), (byte) =>
-              String.fromCodePoint(byte),
-            ).join(""),
-          )}`
-        )) as Record<string, object>
-      ).default,
-    );
-  }
-  if (templateOptions) {
-    const { code, errors, tips } = compileTemplate(templateOptions);
-    errors.forEach((error) => {
-      console.error(error);
+    log(errors);
+    useStyleTag(code, { id });
+  },
+  inject = (code: string) =>
+    import(
+      `data:text/javascript;base64,${btoa(Array.from(new TextEncoder().encode(code), (byte) => String.fromCodePoint(byte)).join(""))}`
+    ),
+  loadModule = async (filename: string) => {
+    const expressionPlugins: CompilerOptions["expressionPlugins"] = [],
+      id = `data-v-${hash(filename)}`,
+      jsxRuntime = "preserve",
+      module: Record<string, object | string> = {},
+      { descriptor, errors } = parse(await (await fetch(filename)).text(), {
+        filename,
+      });
+    const scoped = descriptor.styles.some(({ scoped }) => scoped);
+    let bindingMetadata;
+    log(errors);
+    ["script", "scriptSetup"].forEach((key) => {
+      const { lang = "" } = (descriptor[key as keyof SFCDescriptor] ??
+        {}) as SFCScriptBlock;
+      if (/[jt]sx$/.test(lang)) expressionPlugins.push("jsx");
+      if (/tsx?$/.test(lang)) expressionPlugins.push("typescript");
     });
-    tips.forEach((tip) => {
-      console.warn(tip);
+    if (scoped) module.__scopeId = id;
+    if (descriptor.script || descriptor.scriptSetup) {
+      if (descriptor.script?.src)
+        descriptor.script.content = await (
+          await fetch(descriptor.script.src)
+        ).text();
+      const {
+        bindings,
+        content,
+        warnings = [],
+      } = compileScript(descriptor, { id, inlineTemplate: true });
+      log(warnings);
+      bindingMetadata = bindings;
+      Object.assign(
+        module,
+        (
+          (await inject(
+            expressionPlugins.length
+              ? transform(content, {
+                  jsxRuntime,
+                  transforms: expressionPlugins as Transform[],
+                }).code
+              : content,
+          )) as Record<string, object>
+        ).default,
+      );
+    }
+    if (descriptor.template) {
+      const { code, errors, tips } = compileTemplate({
+        ast: descriptor.template.ast,
+        compilerOptions: {
+          ...(bindingMetadata && { bindingMetadata }),
+          expressionPlugins,
+        },
+        filename: descriptor.filename,
+        id,
+        scoped,
+        slotted: descriptor.slotted,
+        source: descriptor.template.src
+          ? await (await fetch(descriptor.template.src)).text()
+          : descriptor.template.content,
+        // @ts-expect-error TODO remove expect-error after 3.6
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        vapor: descriptor.vapor,
+      });
+      log(errors);
+      log(tips);
+      Object.assign(
+        module,
+        await inject(
+          expressionPlugins.length
+            ? transform(code, {
+                jsxRuntime,
+                transforms: expressionPlugins as Transform[],
+              }).code
+            : code,
+        ),
+      );
+    }
+    descriptor.styles.forEach((style) => {
+      void addStyle(id, descriptor, style);
     });
-    Object.assign(
-      module,
-      (await import(
-        `data:text/javascript;base64,${btoa(
-          Array.from(new TextEncoder().encode(code), (byte) =>
-            String.fromCodePoint(byte),
-          ).join(""),
-        )}`
-      )) as object,
-    );
-  }
-  descriptor.styles.forEach((style) => {
-    void addStyle(id, descriptor, style);
-  });
-  return module;
-};
+    return module;
+  };
 export default loadModule;
